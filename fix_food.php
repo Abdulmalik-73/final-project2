33ab3599e3f4fb655cad69aa8f1ad3ecf00d6736
Abdulmalik-73 <abdulmalik73@example.com>
2026-04-25 09:44:35 +0300

    Cleanup: Remove unused files (M-Pesa, broken booking, one-time migrations, debug files) and clean .env.example

--- fix_food.php
+++
@@ -1,7 +0,0 @@
-<?php
-$content = file_get_contents('food-booking.php');
-$bad  = "formData.append('uid', '<?php echo (int)(\\[''user_id''] ?? 0); ?>');";
-$good = "formData.append('uid', '<?php echo (int)($_SESSION[\"user_id\"] ?? 0); ?>');";
-$content = str_replace($bad, $good, $content);
-file_put_contents('food-booking.php', $content);
-echo "Done. Count: " . substr_count($content, $good);
